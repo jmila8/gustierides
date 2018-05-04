cf3abfcd0f38340970a4f86fbc9e02e7ec6872b4
Jessie Mila <jessie.mila@gmail.com>
2018-05-04 12:56:47 -0500

    Classes

--- app/src/main/java/edu/gac/jmila/gustierides/homePage.java
+++ app/src/main/java/edu/gac/jmila/gustierides/homePage.java
@@ -3,6 +3,14 @@ package edu.gac.jmila.gustierides;
 /**
  * Created by jessiemila on 4/27/18.
  */
+import android.widget.Button;
 
 public class homePage {
+
+    private Button mWallPost;
+    private Button mSafeRide;
+    private Button mGusBus;
+    private Button mShuttle;
+    private Button mLandToAir;
+
 }
