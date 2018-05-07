eb09dcd0d654df9bd77f62d981aa37b95227c3db
dpham <duymosby@gmail.com>
2018-05-07 12:27:45 -0500

    test push

--- app/src/main/java/edu/gac/jmila/gustierides/safeRides.java
+++ app/src/main/java/edu/gac/jmila/gustierides/safeRides.java
@@ -2,12 +2,10 @@
 package edu.gac.jmila.gustierides;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by jessiemila on 4/27/18.
- */
 
-public class safeRides {
+public class safeRides extends AppCompatActivity{
    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
