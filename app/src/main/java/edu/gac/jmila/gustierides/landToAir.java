0b6c7e4abc7fb0811e9dc77ad20ead849f6268aa
dpham <duymosby@gmail.com>
2018-05-07 13:09:25 -0500

    test push

--- app/src/main/java/edu/gac/jmila/gustierides/landToAir.java
+++ app/src/main/java/edu/gac/jmila/gustierides/landToAir.java
@@ -1,16 +1,31 @@
 
 package edu.gac.jmila.gustierides;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 
 public class landToAir extends AppCompatActivity {
+    private Button mLandtoairWebsite;
 
    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.land_to_air);
+
+        mLandtoairWebsite = (Button)findViewById(R.id.landtoair_website);
+        mLandtoairWebsite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uriUrl = Uri.parse("https://www.landtoairexpress.com/");
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+            }
+        });
     }
 
 }
