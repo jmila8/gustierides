d5b4f46a98612211f5f900ef387ec049f2fadbc9
Jessie Mila <jessie.mila@gmail.com>
2018-05-07 10:42:43 -0500

    LTA xml

--- app/src/main/java/edu/gac/jmila/gustierides/landToAir.java
+++ app/src/main/java/edu/gac/jmila/gustierides/landToAir.java
@@ -4,10 +4,11 @@ package edu.gac.jmila.gustierides;
 import android.os.Bundle;
 
 public class landToAir {
+
    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.land_to_air_activity);
+        setContentView(R.layout.land_to_air);
     }
 
 }
