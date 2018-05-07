e4aea0bb4123855bf94bc99313e40c31b0ec9499
Xuan Le <xle2@gustavus.edu>
2018-05-06 21:08:55 -0500

    Update homePage.java

--- app/src/main/java/edu/gac/jmila/gustierides/homePage.java
+++ app/src/main/java/edu/gac/jmila/gustierides/homePage.java
@@ -3,14 +3,48 @@ package edu.gac.jmila.gustierides;
 /**
  * Created by jessiemila on 4/27/18.
  */
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
-public class homePage {
+
+public class homePage extends AppCompatActivity {
 
     private Button mWallPost;
     private Button mSafeRide;
     private Button mGusBus;
-    private Button mShuttle;
     private Button mLandToAir;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepage_main);
+
+        mGusBus = (Button) findViewById(R.id.gus_bus);
+        mGusBus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mSafeRide = (Button) findViewById(R.id.safe_ride);
+        mSafeRide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mLandToAir = (Button) findViewById(R.id.land_to_air);
+        mLandToAir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
 }
